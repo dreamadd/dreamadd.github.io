93b39c236c8aca14b6a6d0bdbb65d1e26d0b80f1
dreamadd <106020426+dreamadd@users.noreply.github.com>
2022-05-22 22:22:15 +0800

    Update js.js

--- behind/js.js
+++ behind/js.js
@@ -1,23 +1,24 @@
 function getname() {
-let name = prompt('请输入用户名。');
-if(name === "云南白曜") {
-  let mima = prompt('请输入密码。');
-  if(mima === "我是云南白曜") {
-    localStorage.setItem('name', name);
-  } else {
-      getname();
-    }
-} else {
-    if(name === "神秘天王") {
-      let mima = prompt('请输入密码。');
-      if(mima === "我是神秘天王哈哈哈") {
-        localStorage.setItem('name', name);
-      } else {
-          getname();
-        }
+  let name = prompt('请输入用户名。');
+  if(name === "云南白曜") {
+    let mima = prompt('请输入密码。');
+    if(mima === "我是云南白曜") {
+      localStorage.setItem('name', name);
     } else {
-      getname();
+        getname();
       }
+  } else {
+      if(name === "神秘天王") {
+        let mima = prompt('请输入密码。');
+        if(mima === "我是神秘天王哈哈哈") {
+          localStorage.setItem('name', name);
+        } else {
+            getname();
+          }
+      } else {
+        getname();
+        }
 }
+
 
 getname();
