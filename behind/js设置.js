6b73c8dc880497f1d5aa6f8c5001c3a28331065a
dreamadd <106020426+dreamadd@users.noreply.github.com>
2022-05-22 22:08:38 +0800

    Update js设置.js

--- behind/js设置.js
+++ behind/js设置.js
@@ -3,17 +3,17 @@ let name = prompt('请输入用户名。');
 if(name === "云南白曜") {
   let mima = prompt('请输入密码。');
   if(mima === "我是云南白曜") {
-  localStorage.setItem('name', name);
+    localStorage.setItem('name', name);
   } else {
-      getname()
+      getname();
     }
 } else {
     if(name === "神秘天王") {
       let mima = prompt('请输入密码。');
       if(mima === "我是神秘天王哈哈哈") {
-      localStorage.setItem('name', name);
+        localStorage.setItem('name', name);
       } else {
-          getname()
+          getname();
         }
     } else {
       getname();
